4ed4fd05ebd9a09b854c863232a113e19582b2a4
thkiefer <thomas.kiefer@iqs.gv.at>
2021-12-31 21:19:33 +0100

    bugfix perturbate 1 and new version for approach from perturbate 2 but a lot faster

--- 01_R/post-optimize_tsp_linkern_Teil2_v3.R
+++ 01_R/post-optimize_tsp_linkern_Teil2_v3.R
@@ -34,10 +34,17 @@ tour_lengths <- function(x, cyclic = FALSE){# x <- santa_tour[[1]]
              row.names(santa_matrix_full)[ind[-1]])
   }
 }
-
-gen_tour_string <- function(tour){
-  ind <- match(names(tour), row.names(santa_matrix_full))
-  santa_matrix_full[ind]
+gen_tour_string <- function(x){
+  ind <- match(names(x), row.names(santa_matrix_full))
+  nch_perm <- 7
+  return(paste0(names(x)[1],
+                paste0(substring(names(x)[-1], 
+                                 nch_perm-tour_lengths(x, cyclic = FALSE)+1, nch_perm), 
+                       collapse = "")
+  ))
+}
+resample <- function(x, ...){
+  x[sample(1:length(x), ...)]
 }
 
 lapply(santa_tour, TSP::tour_length)
@@ -69,7 +76,7 @@ perturbate <- function(tours){# tours <- santa_tour
   ind_cluster6 <- which(tl[[ind_from]] == 6)[use_cluster]
   
   if(use_cluster == 1) ind_perms <- seq(1, ind_cluster6)
-  if(use_cluster == length(cluster_lengths)) ind_perms <- seq(max(which(tl[[ind_from]] == 6))+1, length(tours[[ind_from]]))
+  if(use_cluster == length(cluster_lengths)) ind_perms <- seq(ind_cluster6+1, length(tours[[ind_from]]))
   if(use_cluster > 1 & use_cluster < length(cluster_lengths)) {
     ind_cluster6_low <- which(tl[[ind_from]] == 6)[use_cluster - 1]
     ind_perms <- seq(ind_cluster6_low + 1, ind_cluster6)
@@ -81,7 +88,68 @@ perturbate <- function(tours){# tours <- santa_tour
     "break"
   } else {
     perms <- perms[substrs != "12"]
-    return(list("perms" = names(perms), "from" = ind_from, "to" = ind_to))
+    return(list("perms" = perms, "from" = ind_from, "to" = ind_to))
+  }
+}
+perturbate.v2 <- function(tours){# tours <- santa_tour
+  
+  tl <- lapply(tours, tour_lengths, cyclic = TRUE)
+  take <- lapply(tl, sum) %>% unlist
+  ind_from <- which.max(take)
+  ind_to <- which.min(take)
+  
+  # cut and move several perms that are already in "to"
+  n_move <- 0
+  tstr_to <- gen_tour_string(tours[[ind_to]])
+  cand <- grep("^12", names(tours[[ind_from]]), value = TRUE, invert = TRUE)
+  cand <- names(which(sapply(sapply(cand, grep, tstr_to), length) == 1))
+  
+  if(length(cand) == 0){
+    "break"
+  } else if(length(cand) < 5){
+    perms <- sample(cand, length(cand), prob = tl[[ind_from]][cand])
+  } else {
+    perms <- sample(cand, 5, prob = tl[[ind_from]][cand])
+  }
+  return(list("perms" = perms, "from" = ind_from, "to" = ind_to))
+}
+perturbate.v3 <- function(tours){# tours <- santa_tour
+  
+  tl <- lapply(tours, tour_lengths, cyclic = TRUE)
+  take <- lapply(tl, sum) %>% unlist
+  ind_from <- resample(which(take == max(take)), 1)
+  ind_to <- resample(which(take == min(take)), 1)
+  
+  # cut and move several perms that are already in "to"
+  n_move <- 0
+  tstr_to <- gen_tour_string(tours[[ind_to]])
+  cand <- grep("^12", names(tours[[ind_from]]), value = TRUE, invert = TRUE)
+  cand <- names(which(sapply(sapply(cand, grep, tstr_to), length) == 1))
+  cand <- cand[tl[[ind_from]][cand] > 1]
+  
+  if(length(cand) == 0){
+    "break"
+  } else {
+    for(cc in seq_along(cand)){
+      perm <- cand[cc]
+      
+      # already in to means, that the dist remains intact after insertion
+      # that also means that there is an index where 
+      # dist(ind, cand) + dist(cand, ind+1) equals dist(ind, ind+1)
+      # this is where cand is inserted
+      new_dists <- santa_matrix_full[names(tours[[ind_to]]), perm] +
+        santa_matrix_full[perm, c(names(tours[[ind_to]])[-1], names(tours[[ind_to]])[1])]
+      ind <- unname(which(tl[[ind_to]] == new_dists))
+      
+      tours[[ind_to]] <- c(tours[[ind_to]][1:ind],
+                           tours[[ind_from]][which(names(tours[[ind_from]]) == perm)],
+                           tours[[ind_to]][(ind+1):length(tours[[ind_to]])])
+      tl[[ind_to]] <- tour_lengths(tours[[ind_to]], cyclic = TRUE) #update
+      
+      tours[[ind_from]] <- tours[[ind_from]][-which(names(tours[[ind_from]]) == perm)]
+    }
+    
+    return(list("tours" = tours, "from" = ind_from))
   }
 }
 
@@ -96,17 +164,18 @@ clo <- paste0(c("-K 1",
                 "-R 1000000000"), collapse = " ")
 
 start <- Sys.time()
-
 for(ii in 1:1000){# ii <- 1
   if(ii == 1) cat("\rii = ", 0, "\tmax = ", mx_inc, "\tno mx = ", length(mxs), "\truntime = ", diff_time(Sys.time(), start))
-  pert <- perturbate(inc)
+  pert <- perturbate.v3(inc)
   if(identical(pert, "break")){
     breaks <- breaks+1
     cat("\rii = ", ii, "\tbreaks = ", breaks, "\truntime = ", diff_time(Sys.time(), start))
   } else {
     cat("\n")
-    child <- inc
-    ind1 <- match(sort(setdiff(attr(child[[pert$from]], "names"), pert$perms)),
+    child <- pert$tours
+    
+    # update pert$from
+    ind1 <- match(sort(attr(child[[pert$from]], "names")),
                   row.names(santa_matrix_full))
     sm1 <- santa_matrix_full[ind1, ind1]
     sm1[sm1 == 7] <- Inf
@@ -119,24 +188,12 @@ for(ii in 1:1000){# ii <- 1
                                      verbose = FALSE
     )
     
-    ind2 <- match(sort(union(attr(child[[pert$to]], "names"), pert$perms)),
-                  row.names(santa_matrix_full))
-    sm2 <- santa_matrix_full[ind2, ind2]
-    sm2[sm2 == 7] <- Inf
-    sm2 <- sm2[order(row.names(sm2)),][, sort(row.names(sm2))]
-    atsp <- ATSP(sm2)
-    child[[pert$to]] <-  solve_TSP(atsp, 
-                                   method = "linkern", 
-                                   as_TSP = TRUE, 
-                                   control = list("clo" = clo), 
-                                   verbose = FALSE)
-   
     mx_child <- lapply(child, tour_lengths, cyclic = TRUE) %>% lapply(., sum) %>% unlist %>% max 
     mxs <- c(mxs, mx_child)
-    if(mx_child < mx_inc) {
-      inc <- child
-      mx_inc <- mx_child
-    }
+    
+    inc <- child
+    mx_inc <- mx_child
+    
     cat("\rii = ", ii, "\tmax = ", mx_child, "\tno mx = ", length(mxs), "\truntime = ", diff_time(Sys.time(), start))
   }
   
